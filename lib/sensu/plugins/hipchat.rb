654720b29f1bec18b76eb7d2fbb91f25c56d77e4
Matt Jones <mjones@yieldbot.com>
2015-02-09 10:49:54 -0500

    initial commit

---
+++ lib/sensu/plugins/hipchat.rb
@@ -0,0 +1,9 @@
+require "sensu/plugins/hipchat/version"
+
+module Sensu
+  module Plugins
+    module Hipchat
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/sensu/plugins/hipchat/version.rb
@@ -0,0 +1,7 @@
+module Sensu
+  module Plugins
+    module Hipchat
+      VERSION = "0.0.1"
+    end
+  end
+end
